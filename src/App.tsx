adc934196aff7aeef3fcc26388b8208246b3f37c
Gangadhara Naik R <gnayak025@gmail.com>
2022-09-22 23:14:42 +0530

    Added required dependency Created top, menu and router Added initial pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import ResponsiveHeader from './components/Header';
+import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
+import Home from './pages/Home';
+import AddUpdateImage from './pages/AddImage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      
+      <Router>
+      <ResponsiveHeader />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/addimage" element={<AddUpdateImage/>} />
+      </Routes>
+    </Router>
+        
+        {/* <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
@@ -17,8 +29,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
+        </a> */}
     </div>
   );
 }
